dd15f118c39443b3cfe8e0ba610cb13fb3ad6adb
The Anh <theanh0512@gmail.com>
2017-07-11 17:52:03 +0800

    added crashlytics

--- app/src/main/java/ultimate/fit/ultimatefit/ui/MainActivity.java
+++ app/src/main/java/ultimate/fit/ultimatefit/ui/MainActivity.java
@@ -32,6 +32,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -41,6 +42,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import io.fabric.sdk.android.Fabric;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
@@ -129,6 +131,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
